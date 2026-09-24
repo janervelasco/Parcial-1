39d1adde524f47b5eb4c931c3801e853aba5311a
andres avendaño <andresf.avendanol@uqvirtual.edu.co>
2026-09-24 16:45:32 -0500

    actualizar información terminado

--- parcial_1/src/Main.java
+++ parcial_1/src/Main.java
@@ -19,6 +19,9 @@ public class Main {
                     "\n 5. Calcular ingresos totales." +
                     "\n 6. asignar proyecto a cliente" +
                     "\n 7. lista de proyectos" +
+                    "\n 8. Actualizar información" +
+                    "\n 9. lista de proyectos" +
+                    "\n 10. lista de proyectos" +
                     "\n 0. Salir del sistema"));
 
 
@@ -50,6 +53,40 @@ public class Main {
                 case 7:
                     listarProyectosMain();
                     break;
+                case 8:
+                    int option1 =0;
+                    do{
+                        option1  = Integer.parseInt(JOptionPane.showInputDialog("para actualizar información"+
+                                "\nseleccione una opción:"+
+                                "\n 1. Actualizar información del cliente." +
+                                "\n 2. Actualizar información del desarrollador." +
+                                "\n 3. Actualizar información del proyecto." +
+                                "\n 4. Actualizar información del servicio adicional." +
+                                "\n 0. Salir del sistema"));
+                        switch (option1){
+                            case 1:
+                                actualizarInformacionCliente();
+                                break;
+                            case 2:
+                                actualizarInformacionDesarrollador();
+                                break;
+                            case 3:
+                                actualizarInformacionProyecto();
+                                break;
+                            case 4:
+                                actualizarInformacionServicioAdicional();
+                                break;
+                            case 0:
+                                JOptionPane.showMessageDialog(null, "El programa finalizo");
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "La opcion no es valida");
+                                break;
+
+                        }
+                    }while (option != 0);
+                    break;
+
 
 
 
@@ -73,6 +110,7 @@ public class Main {
         } while (option != 0);
     }
 
+    //-------------------------------------------------------------------------------------------
 
     //Registrar la informacion del cliente
     private static void registrarCliente() {
@@ -91,6 +129,26 @@ public class Main {
             JOptionPane.showMessageDialog(null, "no se hizo el registro");
        }
     }
+
+    //Actualizar información del cliente
+    private static void actualizarInformacionCliente() {
+
+        String documentoClienteActualizar = JOptionPane.showInputDialog("Ingrese el documento del cliente que quiere actualizar: ");
+        String nombre = JOptionPane.showInputDialog("Ingrese el nombre completo del cliente: ");
+        String telefono = JOptionPane.showInputDialog("Ingrese el telefono: ");
+        String email = JOptionPane.showInputDialog("Ingrese el email: ");
+        String pais = JOptionPane.showInputDialog("Ingrese el pais de procedencia: ");
+
+        boolean resultadoActualizacion = miEmpresa.actualizarCliente(documentoClienteActualizar,nombre,telefono,email,pais);
+
+        if(resultadoActualizacion){
+            JOptionPane.showMessageDialog(null, "Se modifico el cliente.");
+        }else{
+            JOptionPane.showMessageDialog(null, "NO se pudo modificar el cliente.");
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------
 
     //REGISTRAR LA INFORMACIÓN DEL DESARROLLADOR
     private static void registrarDesarrolador(){
@@ -112,6 +170,29 @@ public class Main {
         }
     }
 
+    //Actualizar información del desarrollador
+    private static void actualizarInformacionDesarrollador() {
+
+        String codigoDesarrolladorActualizar = JOptionPane.showInputDialog("Ingrese el codigo del desarrollador que quiere actualizar: ");
+        String equipoTrabajo = JOptionPane.showInputDialog("Ingrese el equipo de trabajo: ");
+        String nivel = JOptionPane.showInputDialog("Ingrese el nivel: ");
+        int maxProyectosSimultaneos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el maximo de proyectos simultaneos: "));
+        double tarifaPorDia = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el pais de procedencia: "));
+        String estado = JOptionPane.showInputDialog("ingrese el estado: ");
+
+        boolean resultadoActualizacion = miEmpresa.actualizarDesarrollador(codigoDesarrolladorActualizar,equipoTrabajo,nivel,maxProyectosSimultaneos,tarifaPorDia,estado);
+
+        if(resultadoActualizacion){
+            JOptionPane.showMessageDialog(null, "Se modifico el cliente.");
+        }else{
+            JOptionPane.showMessageDialog(null, "NO se pudo modificar el cliente.");
+        }
+    }
+
+
+
+    //-------------------------------------------------------------------------------------------
+
     //REGISTRAR LA INFORMACIÓN DEL PROYECTO
     private static void registrarProyecto(){
         String codigo = JOptionPane.showInputDialog("ingrese el codigo de proyecto");
@@ -131,6 +212,29 @@ public class Main {
         }
     }
 
+    //Actualizar información del proyecto
+    private static void actualizarInformacionProyecto() {
+
+        String codigoActualizar = JOptionPane.showInputDialog("ingrese el codigo de proyecto que quiere actualizar: ");
+        String fechaSolicitud = JOptionPane.showInputDialog("ingrese la fecha de solicitud");
+        String fechaInicio = JOptionPane.showInputDialog("ingrese la fecha de inicio");
+        String fechaEntrega = JOptionPane.showInputDialog("ingrese la fecha de entrega");
+        String estado = JOptionPane.showInputDialog("ingrese el estado");
+        String metodoPago= JOptionPane.showInputDialog("ingrrese el metodo de pago");
+        double valorTotal = Double.parseDouble(JOptionPane.showInputDialog("ingrese el valor total"));
+
+        boolean resultadoActualizacion = miEmpresa.actualizarProyecto(codigoActualizar,fechaSolicitud,fechaInicio,fechaEntrega,estado,
+                metodoPago,valorTotal);
+
+        if(resultadoActualizacion){
+            JOptionPane.showMessageDialog(null, "Se modifico el cliente.");
+        }else{
+            JOptionPane.showMessageDialog(null, "NO se pudo modificar el cliente.");
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------
+
     //REGISTRAR SERVICIO ADICIONAL
     private static void registrarServicioAdicional(){
         String codigo = JOptionPane.showInputDialog("ingrese el codigo del servicioAdicional");
@@ -146,6 +250,26 @@ public class Main {
             JOptionPane.showMessageDialog(null, "no se hizo el registro");
         }
     }
+
+    //Actualizar información del servicio adicional
+    private static void actualizarInformacionServicioAdicional() {
+
+        String codigoActualizar = JOptionPane.showInputDialog("ingrese el codigo del servicioAdicional que quiere actualizar: ");
+        String nombre = JOptionPane.showInputDialog("ingrese el nombre: ");
+        String descripcion = JOptionPane.showInputDialog("descripción: ");
+        double precio = Double.parseDouble(JOptionPane.showInputDialog("ingrese el precio: "));
+        boolean disponible = Boolean.parseBoolean(JOptionPane.showInputDialog("ingrese la disponibilidad: "));
+
+        boolean resultadoActualizacion = miEmpresa.actualizarServicioAdicional(codigoActualizar,nombre,descripcion,precio,disponible);
+
+        if(resultadoActualizacion){
+            JOptionPane.showMessageDialog(null, "Se modifico el cliente.");
+        }else{
+            JOptionPane.showMessageDialog(null, "NO se pudo modificar el cliente.");
+        }
+    }
+    //--------------------------------------------------------------------------------------------------------------------
+
     private static void calcularIngresosTotales() {
         double total = miEmpresa.calcularIngresosTotales();
         JOptionPane.showMessageDialog(null, "Los ingresos totales de la empresa son: " + total);
--- parcial_1/src/empresa.java
+++ parcial_1/src/empresa.java
@@ -27,7 +27,7 @@ public class empresa{
         listServiciioAdcional = new servicioAdicional[10];
     }
 
-
+//--------------------------------------------------------------------------------------------------------
 
     // REGISTRAR CLIENTE
     public boolean agregarCliente(String documento, String nombreCompleto, String telefono,
@@ -54,8 +54,24 @@ public class empresa{
         return -1;
     }
 
+    //ACTUALIZAR INFORMACIÓN DEL CLIENTE
+    public boolean actualizarCliente(String documentoClienteActualizar,String nombre,String telefono,String email,String pais) {
+        boolean respuesta = false;
+        int index = encontrarIndexCliente(documentoClienteActualizar);
 
+        if (index != -1) {
+            listClientes[index].setNombreCompleto(nombre);
+            listClientes[index].setTelefono(telefono);
+            listClientes[index].setEmail(email);
+            listClientes[index].setPaisProcedencia(pais);
 
+            respuesta = true;
+        }
+
+        return respuesta;
+    }
+
+//--------------------------------------------------------------------------------------------------------
     //REGISTRAR DESARROllADOR
     public boolean agregarDesarrolador(String codigo, String equipoTrabajo, String nivel,
                               int maxProyectosSimultaneos, double tarifaPorDia, String estado){
@@ -81,7 +97,24 @@ public class empresa{
         return -1;
     }
 
+    //ACTUALIZAR INFORMACIÓN DEL DESARROLLADOR
+    public boolean actualizarDesarrollador(String codigoDesarrolladorActualizar,String equipoTrabajo,String nivel,int maxProyectos,double tarifaPorDia,String estado) {
+        boolean respuesta = false;
+        int index = encontrarIndexDesarrollador(codigoDesarrolladorActualizar);
 
+        if (index != -1) {
+            listDesarrolladores[index].setEquipoTrabajo(equipoTrabajo);
+            listDesarrolladores[index].setNivel(nivel);
+            listDesarrolladores[index].setMaxProyectosSimultaneos(maxProyectos);
+            listDesarrolladores[index].setTarifaPorDia(tarifaPorDia);
+            listDesarrolladores[index].setEstado(estado);
+
+            respuesta = true;
+        }
+
+        return respuesta;
+    }
+//--------------------------------------------------------------------------------------------------------
 
     //REGISTRAR PROYECTO
     public boolean agregarProyecto(String codigo, String fechaSolicitud, String fechaInicio,String fechaEntrega,String estado,
@@ -108,6 +141,26 @@ public class empresa{
         return -1;
     }
 
+    //ACTUALIZAR INFORMACIÓN DEL PROYECTO
+    public boolean actualizarProyecto(String codigo, String fechaSolicitud, String fechaInicio,String fechaEntrega,String estado,
+                                      String metodoPago,double valorTotal) {
+        boolean respuesta = false;
+        int index = encontrarIndexProyecto(codigo);
+
+        if (index != -1) {
+            listProyectos[index].setFechaSolicitud(fechaSolicitud);
+            listProyectos[index].setFechaInicio(fechaInicio);
+            listProyectos[index].setFechaEntrega(fechaEntrega);
+            listProyectos[index].setEstado(estado);
+            listProyectos[index].setMetodoPago(metodoPago);
+            listProyectos[index].setValorTotal(valorTotal);
+
+            respuesta = true;
+        }
+
+        return respuesta;
+    }
+//--------------------------------------------------------------------------------------------------------
 
 
     //REGISTRAR SERVICIO ADICIONAL
@@ -134,6 +187,26 @@ public class empresa{
         }
         return -1;
     }
+
+    //ACTUALIZAR INFORMACIÓN DEL PROYECTO
+    public boolean actualizarServicioAdicional(String codigoActualizar,String nombre,String descripcion,double precio,boolean disponible) {
+        boolean respuesta = false;
+        int index = encontrarIndexServicioAdicional(codigoActualizar);
+
+        if (index != -1) {
+            listServiciioAdcional[index].setNombre(nombre);
+            listServiciioAdcional[index].setDescripcion(descripcion);
+            listServiciioAdcional[index].setPrecio(precio);
+            listServiciioAdcional[index].setDisponible(disponible);
+            
+            respuesta = true;
+        }
+
+        return respuesta;
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+
     // CALCULAR INGRESOS TOTALES
     public double calcularIngresosTotales() {
         double ingresosTotales = 0;
@@ -154,6 +227,9 @@ public class empresa{
 
         return ingresosTotales;
     }
+
+    //--------------------------------------------------------------------------------------------------------
+
     public boolean asignarProyectoACliente(String documento, String codigoProyecto) {
         cliente clienteEncontrado = null;
         proyecto proyectoEncontrado = null;
